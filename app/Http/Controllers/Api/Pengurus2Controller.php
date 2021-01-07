3c321adda2e91ac604705a0be5bc3bbacb07cbc8
arif cahyady <arifcihoy@gmail.com>
2021-01-07 09:51:51 +0700

    fix pengurus2

--- app/Http/Controllers/Api/Pengurus2Controller.php
+++ app/Http/Controllers/Api/Pengurus2Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Pengurus2;
+use App\Models\Kas;
 use App\Model\Sampah;
 use Carbon\Carbon;
 
